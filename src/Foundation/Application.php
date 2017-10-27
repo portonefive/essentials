47c88f24057f19776222c2f2e1bf7b8eff0d5df3
Skyler <skyler@portonefive.com>
2017-10-27 11:16:34 -0600

    Update to 5.5

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -3,8 +3,9 @@
 namespace PortOneFive\Essentials\Foundation;
 
 use Illuminate\Events\EventServiceProvider;
-use Illuminate\Foundation\Application as BaseApplication;
+use Illuminate\Log\LogServiceProvider;
 use PortOneFive\Essentials\Routing\RoutingServiceProvider;
+use Illuminate\Foundation\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
@@ -16,6 +17,8 @@ class Application extends BaseApplication
     protected function registerBaseServiceProviders()
     {
         $this->register(new EventServiceProvider($this));
+
+        $this->register(new LogServiceProvider($this));
 
         $this->register(new RoutingServiceProvider($this));
     }
